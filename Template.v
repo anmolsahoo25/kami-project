be1165f84fcaf9d0196de50491058189c2a4ab90
Anmol Sahoo <anmol.sahoo25@gmail.com>
2019-04-28 12:00:39 +0530

    Updated with deps

---
+++ Template.v
@@ -0,0 +1,10 @@
+Require Import Kami.
+(* begin hide *)
+Require Import Kami.Synthesize.
+Require Import Ex.Fifo Ex.NativeFifo Ex.SimpleFifoCorrect.
+Require Import Ext.BSyntax.
+Require Import ExtrOcamlBasic ExtrOcamlNatInt ExtrOcamlString.
+(* end hide *)
+
+Set Implicit Arguments.
+Set Asymmetric Patterns.
---
+++ setup.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+git clone https://github.com/mit-plv/kami
+cd kami; make
